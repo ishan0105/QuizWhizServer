5aeba9c4a038f804e913186ec6ca03f84f249af3
pci20 <pci20@PCI20>
2024-06-26 18:01:10 +0530

    Searching Added in Filter API

--- QuizWhiz.Application/DTOs/Request/GetQuizFilterDTO.cs
+++ QuizWhiz.Application/DTOs/Request/GetQuizFilterDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,18 @@ namespace QuizWhiz.Application.DTOs.Request
 {
     public class GetQuizFilterDTO
     {
+        public string SearchValue { get; set; } = string.Empty;
+
+        [Required]
         public required int StatusId { get; set; }
 
+        [Required]
         public required int DifficultyId { get; set; }
 
+        [Required]
         public required int CategoryId { get; set; }
 
+        [Required]
         public required int CurrentPage { get; set; }
     }
 }
