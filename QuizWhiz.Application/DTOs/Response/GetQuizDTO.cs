7386e36408bc83d61f8e05f8a4ba5d2819eca227
pci20 <pci20@PCI20>
2024-07-02 15:37:55 +0530

    Quiz Id Changed

--- QuizWhiz.Application/DTOs/Response/GetQuizDTO.cs
+++ QuizWhiz.Application/DTOs/Response/GetQuizDTO.cs
@@ -9,19 +9,15 @@ namespace QuizWhiz.Application.DTOs.Response
     public class GetQuizDTO
     {
         public int QuizId { get; set; }
-
         public string? Title { get; set; } = null;
-
         public string? Description { get; set; } = null;
-
         public int? CategoryId { get; set; } = null;
-
         public string? CategoryName { get; set; } = null;
-
+        public string TotalMarks { get; set; } = null;
+        public int WinningMarks { get; set; } = 0;
+        public string NegativeMarks { get; set; } = null;
         public int? DifficultyId { get; set; } = null;
-
         public DateTime ScheduledDate { get; set; }
-
         public string? QuizLink { get; set; } = null;
     }
 }
