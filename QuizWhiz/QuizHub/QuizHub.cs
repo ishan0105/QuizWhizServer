193df724b3d1f0b92157af0363201e375ceda8c0
Ishan Bhatt <tatva.dotnet.ishanbhatt@outlook.com>
2024-07-11 14:46:08 +0530

    created signalR methods

--- QuizWhiz/QuizHub/QuizHub.cs
+++ QuizWhiz/QuizHub/QuizHub.cs
@@ -1,21 +1,60 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System;
 using System.Threading.Tasks;
-using Npgsql;
 using QuizWhiz.Application.Interface;
 using QuizWhiz.Application.DTOs.Request;
+using System.Timers;
 
 public class QuizHub : Hub
 {
     private readonly IQuizService _quizService;
+    private static System.Timers.Timer _timer;
+    private static bool _timerInitialized = false;
+    private static object _lock = new object();
+
     public QuizHub(IQuizService quizService)
     {
         _quizService = quizService;
     }
 
-    //public async Task SendMessage(string user, string message)
-    //{
-    //    await Clients.All.SendAsync("ReceiveMessage", user, message);
-    //}
+    public override async Task OnConnectedAsync()
+    {
+        await base.OnConnectedAsync();
+        Console.WriteLine($"Client connected: {Context.ConnectionId}");
+
+        lock (_lock)
+        {
+            if (!_timerInitialized)
+            {
+                _timer = new System.Timers.Timer(5000); // 5 seconds interval
+                _timer.Elapsed += TimerElapsed;
+                _timer.Start();
+                _timerInitialized = true;
+            }
+        }
+    }
+
+    public override async Task OnDisconnectedAsync(Exception exception)
+    {
+        await base.OnDisconnectedAsync(exception);
+        Console.WriteLine($"Client disconnected: {Context.ConnectionId}");
+
+        lock (_lock)
+        {
+            if (Clients.All == null)
+            {
+                _timer.Stop();
+                _timer.Dispose();
+                _timerInitialized = false;
+            }
+        }
+    }
+
+    private void TimerElapsed(object sender, ElapsedEventArgs e)
+    {
+        // Send a message to all connected clients to fetch the next question
+        Clients.All.SendAsync("FetchNextQuestion");
+    }
 
     public async Task GetNewQuestion(string quizLink, int questionCount)
     {
@@ -35,7 +74,6 @@ public class QuizHub : Hub
             response.Message = "Question does not exists";
             await Clients.Caller.SendAsync("ReceiveError", response.Message);
         }
-
     }
 
     public async Task GetCorrectAnswer(string quizLink, int questionCount)
