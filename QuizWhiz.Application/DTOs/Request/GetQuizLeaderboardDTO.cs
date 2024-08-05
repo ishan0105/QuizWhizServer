6b76ff23e56aa1f090eade1693b84efd95b3f923
Hitb23 <hit.bambhroliya@etatvasoft.com>
2024-08-05 11:41:43 +0530

    Update Questions Updated

---
+++ QuizWhiz.Application/DTOs/Request/GetQuizLeaderboardDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWhiz.Application.DTOs.Request
+{
+    public class GetQuizLeaderboardDTO
+    {
+        [Required]
+        public string QuizLink { get; set; } = string.Empty;
+
+        public string SearchedWord { get; set; } = string.Empty;
+
+        [Required]
+        public required int CurrentPage { get; set; }
+
+        [Required]
+        public required int PageSize { get; set; }
+    }
+}
---
+++ QuizWhiz.Application/DTOs/Response/QuizParticipantsDTO.cs
@@ -0,0 +1,14 @@
+﻿using QuizWhiz.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizWhiz.Application.DTOs.Response
+{
+    public class QuizParticipantsDTO
+    {
+      public List<ParticipantsDetailsDTO>? participantsList {  get; set; }   
+    }
+}
